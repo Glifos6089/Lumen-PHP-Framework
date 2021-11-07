923b7b176f2479fc72a5a926173755de1b695ec8
GlifosAFM <42324647+Glifos6089@users.noreply.github.com>
2021-11-07 16:34:39 -0500

    parte 9

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -29,6 +29,6 @@ class Controller extends BaseController
         $totalQuotes = (count(Controller::$quotes));
         $randomNumber = (rand(0, ($totalQuotes - 1)));
         $randomQuote = Controller::$quotes[$randomNumber];
-        return '<p>'.gethostbyname(gethostename()).'</p>';
+        return response("<p>".gethostbyname(gethostename())."</p>");
     }
 }
